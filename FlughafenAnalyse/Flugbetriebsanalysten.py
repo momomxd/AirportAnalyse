dc6b70236cd30f426d2235363e52356597f9fdf9
momomxd <148530510+momomxd@users.noreply.github.com>
2024-01-17 08:50:42 +0100

    Update Flugbetriebsanalysten.py

--- FlughafenAnalyse/Flugbetriebsanalysten.py
+++ FlughafenAnalyse/Flugbetriebsanalysten.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 
 # Daten laden
 flights = pd.read_csv('Dataset/flights.csv')
-airports = pd.read_csv('Dataset/airports.csv')  # Stellen Sie sicher, dass diese Datei die Namen der Flughäfen enthält
+airports = pd.read_csv('Dataset/airports.csv')  
 
-# Stellen Sie sicher, dass alle Verspätungsspalten und DEPARTURE_DELAY korrekt formatiert sind
 delay_columns = ['WEATHER_DELAY', 'AIR_SYSTEM_DELAY', 'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY']
 for column in delay_columns + ['DEPARTURE_DELAY']:
     flights[column] = pd.to_numeric(flights[column].fillna(0))
 
-# Ermitteln Sie die Top 5 Flughäfen mit den meisten Verspätungen
+# Top 5 Flughäfen mit den meisten Verspätungen
 top_airports_delays = flights.groupby('ORIGIN_AIRPORT')['DEPARTURE_DELAY'].sum().nlargest(5)
 top_airports = top_airports_delays.index
 
@@ -21,7 +20,7 @@ airport_labels = [f"{code} - {name}" for code, name in airport_names['AIRPORT'].
 # Farbpalette für die Diagramme auswählen
 colors = plt.cm.Paired(range(len(delay_columns)))
 
-# Erstellen Sie für jeden der Top 5 Flughäfen ein Kuchendiagramm
+# Kuchendiagramm für die Top 5 Flughäfen
 fig, axs = plt.subplots(1, 5, figsize=(20, 8))  # Adjust the size as needed
 
 # Einzelne Kuchendiagramme erstellen
